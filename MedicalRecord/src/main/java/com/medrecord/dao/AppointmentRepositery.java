fcc9e5ab0f07f5263437686884be9259dc494889
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-04-05 17:29:00 +0530

    Patient List api for doctor done

--- MedicalRecord/src/main/java/com/medrecord/dao/AppointmentRepositery.java
+++ MedicalRecord/src/main/java/com/medrecord/dao/AppointmentRepositery.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.medrecord.Entity.Appointment;
 
+import java.util.List;
+
 
 public interface AppointmentRepositery extends JpaRepository<Appointment, Integer> {
 	Appointment findByAppointmentId(int appointmentId);
+	List<Appointment> findByDoctorUsername(String doctorUsername);
 }
